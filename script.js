8e3f1f64e75cabaf44d4e6d03a75a51bb4d1e0b9
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-07-29 11:58:17 -0400

    Add calculation functions in script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function getSum (x, y) {
+  return x + y;
+}
+
+function getDifference (x, y) {
+  return x - y;
+}
+
+function getProduct (x, y) {
+  return x * y;
+}
+
+function getQuotient (x, y) {
+  return x / y;
+}
+
+function getRemainder (x, y) {
+  return x % y;
+}
